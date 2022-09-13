e21e7beef750b9479aa4a8d8c026f085e510e549
Giselle Cordone <cordonegiselle@gmail.com>
2022-09-13 13:43:32 -0300

    instalando swagger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,10 +11,31 @@ const port = process.env.PORT || 3000;
 const userRoutes = require('./routes/userRoutes')
 const taskRoutes = require('./routes/taskRoutes')
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Projeto Final',
+            version: '1.0.0',
+            description: 'API REST de Lista de Tarefas - Operações CRUD'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000'
+            }
+        ],
+    },
+    apis: ['./routes/*.js']
+};
+
+
+const specs = swaggerJsDoc(options)
+
 //middleware
 app.use(express.json())
 app.use('/user', userRoutes);
 app.use('/task', taskRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // mongodb connection
 mongoose
