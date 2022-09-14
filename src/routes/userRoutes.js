6e547b2cd6e65fb1b50eb4e61c92f75ae0cac091
Giselle Cordone <cordonegiselle@gmail.com>
2022-09-14 15:27:42 -0300

    documentando rota novouser

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,33 @@ const router = express.Router();
 const { createUser, getUsers, getUserId, putUser, deleteUser } = require('../controllers/userControllers');
 
 // Criação de user 
+/**
+ * @swagger
+ * /user/novouser:
+ *  post:
+ *      summary: criação de user
+ *      tags: [User]
+ *      requestBody: 
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                          age:
+ *                              type: number
+ *                          telephone: 
+ *                              type: number
+ *                          email:
+ *                              type: string
+ *                          role: 
+ *                              type: string
+ *                  required: true
+ *      responses:
+ *          200:
+ *              description: Task criada com sucesso
+ */
 router.post('/novouser', createUser);
 
 // Listar users
