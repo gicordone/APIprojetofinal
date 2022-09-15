eb4b19d0f6bc3e403e062389c23811aa094602b4
Giselle Cordone <cordonegiselle@gmail.com>
2022-09-14 21:42:27 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,23 +2,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsDoc = require('./swagger');
+const userRoutes = require('./routes/userRoutes')
+const taskRoutes = require('./routes/taskRoutes')
 const path = require('path');
 
-//  settings
+// swagger 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Projeto Final',
+            version: '1.0.0',
+            description: 'API REST de Lista de Tarefas - Operações CRUD'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000'
+            }
+        ]
+    },
+    apis: [`${path.join(__dirname, './routes/*.js')}`]
+};
+
+// settings
 const app = express();
 const port = process.env.PORT || 3000;
 
-const userRoutes = require('./routes/userRoutes')
-const taskRoutes = require('./routes/taskRoutes');
-const { options } = require('./routes/userRoutes');
-
-//middleware
+//middleware`
 app.use(express.json())
 app.use('/user', userRoutes);
 app.use('/task', taskRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(options)));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerSpec)));
 
 // mongodb connection
 mongoose
